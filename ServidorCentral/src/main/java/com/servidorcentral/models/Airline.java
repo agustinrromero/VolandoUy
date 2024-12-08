ea612d634c638a2a5153b35d34ef1ff3a2c9f79f
agustinrromero <agustinriveroromero@gmail.com>
2024-12-08 20:41:41 -0300

    add flight routes to 'Airline' entity

--- ServidorCentral/src/main/java/com/servidorcentral/models/Airline.java
+++ ServidorCentral/src/main/java/com/servidorcentral/models/Airline.java
@@ -1,11 +1,13 @@
 package com.servidorcentral.models;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 public class Airline extends User {
 
     private String description;
     private String webUrl;
+    private Set<FlightRoute> flightRoutes;
 
     public Airline(String username, String name, String email, String password, byte[] image, LocalDate registrationDate, String description, String webUrl) {
         super(username, name, email, password, image, registrationDate);
