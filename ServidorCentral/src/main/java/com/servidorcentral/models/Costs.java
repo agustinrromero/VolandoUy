64858197e866bc5128e92a323429948f3f341e2c
agustinrromero <agustinriveroromero@gmail.com>
2024-12-08 18:10:43 -0300

    add 'costs' entity
    - Added base cost for tourist and business class, and cost per unit of extra luggage
    - Used BigDecimal for all attributes
    - Added constructor and getters

---
+++ ServidorCentral/src/main/java/com/servidorcentral/models/Costs.java
@@ -0,0 +1,30 @@
+package com.servidorcentral.models;
+
+import java.math.BigDecimal;
+
+public class Costs {
+
+    private BigDecimal baseCostTouristClass;
+    private BigDecimal baseCostBusinessClass;
+    private BigDecimal costPerUnitExtraLuggage;
+
+    public Costs(BigDecimal baseCostTouristClass, BigDecimal baseCostBusinessClass, BigDecimal costPerUnitExtraLuggage) {
+        this.baseCostTouristClass = baseCostTouristClass;
+        this.baseCostBusinessClass = baseCostBusinessClass;
+        this.costPerUnitExtraLuggage = costPerUnitExtraLuggage;
+    }
+
+    public BigDecimal getBaseCostTouristClass() {
+        return baseCostTouristClass;
+    }
+
+    public BigDecimal getBaseCostBusinessClass() {
+        return baseCostBusinessClass;
+    }
+
+    public BigDecimal getCostPerUnitExtraLuggage() {
+        return costPerUnitExtraLuggage;
+    }
+
+}
+
