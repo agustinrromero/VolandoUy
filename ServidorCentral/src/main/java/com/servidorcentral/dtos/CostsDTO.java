bc9b2652dde58bc0259613a2047854fc1713393b
agustinrromero <agustinriveroromero@gmail.com>
2024-12-13 18:24:23 -0300

    feat: add method to get DTO in Costs

--- ServidorCentral/src/main/java/com/servidorcentral/dtos/CostsDTO.java
+++ ServidorCentral/src/main/java/com/servidorcentral/dtos/CostsDTO.java
@@ -1,10 +1,22 @@
 package com.servidorcentral.dtos;
 
+import com.servidorcentral.models.Costs;
+
 import java.math.BigDecimal;
 
 public record CostsDTO(
         BigDecimal baseCostTouristClass,
         BigDecimal baseCostBusinessClass,
         BigDecimal costPerUnitExtraLuggage
-) {}
+) {
+
+    public CostsDTO(Costs costs) {
+        this(
+                costs.getBaseCostTouristClass(),
+                costs.getBaseCostBusinessClass(),
+                costs.getCostPerUnitExtraLuggage()
+        );
+    }
+
+}
 
