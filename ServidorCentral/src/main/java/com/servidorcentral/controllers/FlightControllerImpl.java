ad60bbb85484954947465e41f8ed4f0dda6830db
agustinrromero <agustinriveroromero@gmail.com>
2024-12-19 03:19:44 -0300

    feat(FlightController): add method to retrieve all categories' names

--- ServidorCentral/src/main/java/com/servidorcentral/controllers/FlightControllerImpl.java
+++ ServidorCentral/src/main/java/com/servidorcentral/controllers/FlightControllerImpl.java
@@ -2,7 +2,10 @@ package com.servidorcentral.controllers;
 
 import com.servidorcentral.exceptions.CityAlreadyExistsException;
 import com.servidorcentral.models.City;
+import com.servidorcentral.repositories.CategoryRepository;
 import com.servidorcentral.repositories.CityRepository;
+
+import java.util.Set;
 
 public class FlightControllerImpl implements FlightController {
 
@@ -10,6 +13,12 @@ public class FlightControllerImpl implements FlightController {
     public void createCity(City city) throws CityAlreadyExistsException {
         CityRepository cityRepository = CityRepository.getInstance();
         cityRepository.addCity(city);
+    }
+
+    @Override
+    public Set<String> getAllCategories() {
+        CategoryRepository categoryRepository = CategoryRepository.getInstance();
+        return categoryRepository.getAllCategories();
     }
 
 }
