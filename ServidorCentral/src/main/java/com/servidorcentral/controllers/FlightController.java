497de790ad80c1bd7973bcbd0272b646b45cf1fb
agustinrromero <118568147+agustinrromero@users.noreply.github.com>
2024-12-19 03:20:20 -0300

    feat(FlightController): add method to retrieve all categories' names

--- ServidorCentral/src/main/java/com/servidorcentral/controllers/FlightController.java
+++ ServidorCentral/src/main/java/com/servidorcentral/controllers/FlightController.java
@@ -3,9 +3,13 @@ package com.servidorcentral.controllers;
 import com.servidorcentral.exceptions.CityAlreadyExistsException;
 import com.servidorcentral.models.City;
 
+import java.util.Set;
+
 public interface FlightController {
 
     void createCity(City city) throws CityAlreadyExistsException;
+
+    Set<String> getAllCategories();
 
 }
 
